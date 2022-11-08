6fe85d4f2753de77d27e205a75379939163b817e
JosephW143 <josephwright1436@gmail.com>
2022-11-08 17:24:42 -0500

    Footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-info flex-row align-center ">
+            <nav className="text-center">
+                <a href='https://github.com/JosephW143'>
+                    GitHub
+                </a>
+
+                <a href='www.linkedin.com/in/joseph-a-wright-14362iw4e5tu'>
+                    Linkedin
+                </a>
+
+                <a href='mailto:josephwright1436@gmail.com'>
+                    Email
+                </a>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer; 
