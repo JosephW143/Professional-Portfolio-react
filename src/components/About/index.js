47fddc253c73279b3686a538f5025bb6e6251c1c
JosephW143 <josephwright1436@gmail.com>
2022-11-09 16:35:52 -0500

    update readme

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ function About () {
             </div>
             <div className="container">
                 <div className="row">
-                    <div className="col-6 text-right">
+                    <div className="col-6 text-right" id="aboutTxt">
                         <p>
                             I am a up and coming developer who is very eager to work in the industry. At the UCF full stack developer Boot Camp I have leaned many langagues and technologies including HTML5, CSS, JavaScript, jQuery, MySQL, Express.js, and Node.js to name a few. Here are a few samples of my work. You can checkout the rest of my works at my GitHub that's linked at the bottom of the page. There you can also find my other contact information. Thank you for visiting my portfolio!
                         </p>
