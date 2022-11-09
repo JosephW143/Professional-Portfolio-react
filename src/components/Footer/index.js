3e2b23132b4cf9d0b1c818d0a8deb1a48bb8c080
JosephW143 <josephwright1436@gmail.com>
2022-11-09 13:23:59 -0500

    clean up footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,18 +3,26 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="bg-info flex-row align-center ">
-            <nav className="text-center">
-                <a href='https://github.com/JosephW143'>
-                    GitHub
-                </a>
+            <nav className="mx-auto p-3">
+                <ul className="list-inline">
+                    <li className="list-inline-item">
+                        <a href='https://github.com/JosephW143'>
+                            GitHub
+                        </a>
+                    </li>
 
-                <a href='www.linkedin.com/in/joseph-a-wright-14362iw4e5tu'>
-                    Linkedin
-                </a>
+                    <li className="list-inline-item">
+                        <a href='www.linkedin.com/in/joseph-a-wright-14362iw4e5tu'>
+                            Linkedin
+                        </a>
+                    </li>
 
-                <a href='mailto:josephwright1436@gmail.com'>
-                    Email
-                </a>
+                    <li className="list-inline-item">
+                        <a href='mailto:josephwright1436@gmail.com'>
+                            Email
+                        </a>
+                    </li>
+                </ul>
             </nav>
         </footer>
     );
