543cc7bfd25673629a2d55c1d01ec5b981b5e550
JosephW143 <josephwright1436@gmail.com>
2022-11-09 14:01:04 -0500

    contact components

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,9 +2,21 @@ import React from "react";
 
 function Contact () {
     return (
-        <div>
-
+        <form id='contact'>
+        <div className="form-group">
+          <label for="nameInput">Name</label>
+          <input type="text" className="form-control" id="nameInput" placeholder="Please enter your name" />
         </div>
+        <div className="form-group">
+          <label for="emailInput">Email</label>
+          <input type="email" className="form-control" id="emailInput" placeholder="Please enter your email" />
+        </div>
+        <div className="form-group">
+          <label for="messageInput">Message</label>
+          <textarea className="form-control" id="messageInput" placeholder="Message"></textarea>
+        </div>
+        <button type="submit" className="btn btn-secondary">Submit</button>
+      </form>
     );
 }
 
